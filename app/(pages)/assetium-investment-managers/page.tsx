1e0cc141028f8d3da1a73aaa83600db0b6d3df8b
KPAMsar <kpamsarshija@gmail.com>
2026-07-22 14:02:05 +0100

    fix

--- app/(pages)/assetium-investment-managers/page.tsx
+++ app/(pages)/assetium-investment-managers/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 // import Link from "next/link";
 import AIMLHeader from "@/app/sections/aiml-header";
 import AIMLServices from "@/app/sections/aiml-service";
